60d64fa6a2a8000e7e1f42082a8b3294882ced62
JN <91375942+jnesew@users.noreply.github.com>
2026-08-31 21:35:47 +0000

    feat: export parsing modes

--- src/pubparser/__init__.py
+++ src/pubparser/__init__.py
@@ -12,6 +12,7 @@ from .errors import (
     UnsupportedFeatureError,
     ValidationError,
 )
+from .modes import ParsingMode
 from .models import ContentBlock, Cover, EncryptedResource, EncryptionInfo, ExtractedDocument, Navigation, NavigationEntry, NavigationList, NormalizationResult
 from .normalizers import ProjectGutenbergNormalizer, normalize_project_gutenberg
 from .resources import Resource, ResourceCollection
@@ -20,6 +21,6 @@ from .security import DEFAULT_LIMITS, SecurityLimits
 __all__ = [
     "ArchiveError", "ContainerError", "ContentBlock", "Cover", "DEFAULT_LIMITS", "Diagnostic", "EpubBook",
     "EncryptedResource", "EncryptionInfo", "EpubError", "ExtractedDocument", "InvalidArchiveError", "Navigation", "NavigationEntry", "NavigationError",
-    "NavigationList", "NormalizationResult", "PackageError", "ProjectGutenbergNormalizer", "Resource", "ResourceCollection", "ResourceError", "SecurityLimits", "Severity",
+    "NavigationList", "NormalizationResult", "PackageError", "ParsingMode", "ProjectGutenbergNormalizer", "Resource", "ResourceCollection", "ResourceError", "SecurityLimits", "Severity",
     "UnsafeArchiveError", "UnsupportedFeatureError", "ValidationError", "normalize_project_gutenberg", "open_epub",
 ]
